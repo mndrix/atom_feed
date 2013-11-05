f033d530e9a597af1c5bb44a9d15342853543b99
Michael Hendricks <michael@ndrix.org>
2013-11-05 16:00:04 -0700

    Mention dependency on library(delay)

--- pack.pl
+++ pack.pl
@@ -4,6 +4,8 @@ title('Atom Syndication Format').
 version('0.0.1').
 download('http://packs.ndrix.com/atom_feed/atom_feed-0.0.1.tgz').
 
+requires(delay).
+
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
