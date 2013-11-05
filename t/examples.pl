d8dac13ccd1d5721dc4ae3c917692df03c81dcde
Michael Hendricks <michael@ndrix.org>
2013-11-05 15:40:43 -0700

    Tests using some sample feeds
    I downloaded Atom files from a few feeds that I follow.  The samples are
    used for a few test cases.
    
    The samples are also handy during development.  The tidy-*.xml files
    were passed through HTML Tidy with XML settings.  These versions are
    easier to work with during development.  All tests should be run
    against the original files to make sure we can handle real world files.

--- t/examples.pl
+++ t/examples.pl
@@ -1,8 +1,29 @@
 :- use_module(library(atom_feed)).
-
-% define helper predicates here
-
 :- use_module(library(tap)).
 
-% add tests showing common usage
-todo :- fail.
+'xkcd: improved keyboard' :-
+    new_feed(file('samples/xkcd.xml'), Feed),
+    entry(Feed, Entry),
+    id(Entry, 'http://xkcd.com/1284/'),
+    title(Entry, Title),
+    Title == 'Improved Keyboard',
+    !.  % cut search through other entries
+
+
+'github: introducing octokit' :-
+    new_feed(file('samples/github.xml'), Feed),
+    entry(Feed, Entry),
+    title(Entry, 'Introducing Octokit.net'),
+    link(Entry, Link),
+    rel(Link, alternate),
+    href(Link, Url),
+    Url == 'https://github.com/blog/1676-introducing-octokit-net',
+    !.  % cut search through other entries
+
+'blogger: pubsubhubbub' :-
+    new_feed(file('samples/blogger.xml'), Feed),
+    link(Feed, Link),
+    rel(Link, hub),
+    href(Link, Hub),
+    Hub == 'http://pubsubhubbub.appspot.com/',
+    !.  % cut search through other links
